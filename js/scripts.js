9bd6ddb6e3d0910a6747f9da185d50868aaf7404
benchurchwell <ben@benchurchwell.com>
2023-01-28 22:21:23 -0800

    should change langSelect class to be displayed. does not work

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 function handleSelect(event) {
   event.preventDefault();
-  const selection = document.getElementById("questions").value;
-  console.log(selection)
-}
+  const langSelect = document.getElementById("questions").value;
+  console.log(langSelect)
 
+    if langSelect === "python" {
+    setAttribute("class", "python");
+  } else if langSelect === "swift" {
+    setAttribute("class", "swift");
+  } else if langSelect === "golang"{
+    setAttribute("class", "golang");
+  } else {
+    setAttribute("class", "javascript");
+  }
+}
 window.addEventListener("load", function() {
   document.getElementById("select-form").addEventListener("submit", handleSelect);
 });
-
