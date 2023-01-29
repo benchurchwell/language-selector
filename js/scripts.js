677d127f62aa11203c325c33a12786bde639bfbb
benchurchwell <ben@benchurchwell.com>
2023-01-28 21:37:21 -0800

    removed uncesessary event handelers

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,3 @@ function handleSelect(event) {
 window.addEventListener("load", function() {
   document.getElementById("select-form").addEventListener("submit", handleSelect);
 });
-
-function handleRadio(event) {
-  event.preventDefault();
-  const radioSelection = document.querySelector("input[name='flavor']:checked").value;
-}
-
-window.addEventListener("load", function() {
-  document.getElementById("radio-form").addEventListener("submit", handleRadio);
-});
-
-// const dob = document.getElementById("born").value;
-
-// const favoriteColor = document.getElementById("color").value;
